7e02d99fded73d59f66f5c95cff5b074be1f5814
Bot-Kerem <akerem1434@gmail.com>
2023-05-04 18:29:56 +0300

    Added feature: Threading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::path::Path;
+use std::sync;
+use std::thread;
 
 use tinyraytracer::{ray::Ray, vec3::Vector3, Scalar};
 
@@ -8,24 +10,30 @@ const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as Scalar / ASPECT_RATIO) as u32;
 
 const SAVE_PATH: &str = "image.png";
 
+const NUM_THREADS: usize = 4;
+
 fn hit_sphere(center: &Vector3, radius: Scalar, ray: &Ray) -> Scalar {
     let oc = ray.origin - (*center);
-    let a = ray.direction.dot(&ray.direction);
-    let b = 2.0 * oc.dot(&ray.direction);
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b*b - 4.0*a*c;
+    let a = ray.direction.length_squared();
+    let half_b = oc.dot(&ray.direction);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
 
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (-half_b - discriminant.sqrt()) / a
     }
 }
 
 fn send_ray(ray: &Ray) -> Vector3 {
-    let sphere_center = Vector3 { x: 0.0, y: 0.0, z: -1.0};
+    let sphere_center = Vector3 {
+        x: 0.0,
+        y: 0.0,
+        z: -1.0,
+    };
     let t = hit_sphere(&sphere_center, 0.5, &ray);
-    if  t > 0.0 {
+    if t > 0.0 {
         let normal = (ray.at(t) - sphere_center).unit_vector() + Vector3::new(1.0);
         return normal * 0.5;
     }
@@ -33,8 +41,16 @@ fn send_ray(ray: &Ray) -> Vector3 {
     let unit_direction = ray.direction.unit_vector();
     let t = 0.5 * (unit_direction.y + 1.0);
 
-    let sky_start = Vector3 { x: 1.0, y: 1.0, z: 1.0 };
-    let sky_end = Vector3 { x: 0.5, y: 0.7, z: 1.0 };
+    let sky_start = Vector3 {
+        x: 1.0,
+        y: 1.0,
+        z: 1.0,
+    };
+    let sky_end = Vector3 {
+        x: 0.5,
+        y: 0.7,
+        z: 1.0,
+    };
 
     sky_start * (1.0 - t) + sky_end * t
 }
@@ -56,7 +72,8 @@ fn main() {
         y: viewport_height,
         z: 0.0,
     };
-    let lower_left_corner = origin - horizontal / 2.0
+    let lower_left_corner = origin
+        - horizontal / 2.0
         - vertical / 2.0
         - Vector3 {
             x: 0.0,
@@ -64,6 +81,106 @@ fn main() {
             z: focal_length,
         };
 
+    struct Worker {
+        pixel_position: (usize, usize),
+        tx: sync::mpsc::Sender<Ray>,
+        rx: sync::mpsc::Receiver<Vector3>,
+        status: bool,
+    }
+
+    impl Worker {
+        fn new(
+            tx: sync::mpsc::Sender<Ray>,
+            rx: sync::mpsc::Receiver<Vector3>,
+        ) -> Self {
+            Worker {
+                pixel_position: (0, 0),
+                tx,
+                rx,
+                status: true,
+            }
+        }
+    }
+
+    /* Create threads */
+    let mut workers = Vec::new();
+    let mut threads = Vec::new();
+    for _ in 0..NUM_THREADS {
+        let (tx_ray, rx_ray) = sync::mpsc::channel::<Ray>();
+        let (tx_color, rx_color) = sync::mpsc::channel::<Vector3>();
+        let worker = Worker::new(
+            tx_ray,
+            rx_color,
+        );
+
+        workers.push(worker);
+
+        threads.push(thread::spawn(move || {
+                while let Ok(ray) = rx_ray.recv() {
+                    let pixel_color = send_ray(&ray);
+                    tx_color.send(pixel_color).unwrap();
+                }
+            }));
+    }
+
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+
+    /* Send first ray to workers */
+    println!("Sending rays");
+    for worker in &mut workers {
+        worker.pixel_position = (x, y);
+        let u = x as Scalar / (IMAGE_WIDTH - 1) as Scalar;
+        let v = ((IMAGE_HEIGHT - 1) - y as u32) as Scalar / (IMAGE_HEIGHT - 1) as Scalar;
+
+        let ray = Ray::new(
+            origin,
+            lower_left_corner + (horizontal * u) + (vertical * v) - origin,
+        );
+
+        worker.tx.send(ray).unwrap();
+
+        x += 1;
+        if x >= IMAGE_WIDTH as usize {
+            x = 0;
+            y += 1;
+        }
+    }
+
+
+    while workers.iter().any(|w| w.status == true) {
+        /* Main loop */
+        for worker in &mut workers {
+            if let Ok(pixel_color) = worker.rx.try_recv() {
+                buffer[worker.pixel_position.1][worker.pixel_position.0] = pixel_color;
+
+                if y < IMAGE_HEIGHT as usize {
+                    worker.pixel_position = (x, y);
+
+                    let u = x as Scalar / (IMAGE_WIDTH - 1) as Scalar;
+                    let v = ((IMAGE_HEIGHT - 1) - y as u32) as Scalar / (IMAGE_HEIGHT - 1) as Scalar;
+
+                    let ray = Ray::new(
+                        origin,
+                        lower_left_corner + (horizontal * u) + (vertical * v) - origin,
+                    );
+
+                    worker.tx.send(ray).unwrap();
+
+                    x += 1;
+                    if x >= IMAGE_WIDTH as usize {
+                        x = 0;
+                        y += 1;
+                    }
+                } else {
+                    worker.status = false;
+                }
+            }
+        }
+    }
+    println!("Done");
+
+    /*
     for y in 0..buffer.len() {
         for x in 0..buffer[y].len() {
             let u = x as Scalar / (IMAGE_WIDTH - 1) as Scalar;
@@ -77,6 +194,7 @@ fn main() {
             buffer[y][x] = send_ray(&ray);
         }
     }
+    */
 
     println!("Creating image");
     let mut image_buffer = [0u8; (IMAGE_WIDTH * IMAGE_HEIGHT * 3) as usize];
